c7c864975fe070fc4d8f8e137a06a6d36bcc2357
dlgnlwo3 <dlgnlwo3@naver.com>
2022-09-08 08:33:58 +0900

    다시 복구

--- test.py
+++ test.py
@@ -12,11 +12,18 @@ def copy_sheet(old_file, new_file):
         excel.Visible = True
 
         wb1 = excel.Workbooks.Open(old_file)
+        # wb2 = excel.Workbooks.Open(new_file)
         ws1 = wb1.ActiveSheet
         print(f"{wb1}, {ws1}")
 
+        # ws1.Copy(After=wb2.Worksheets(f"Sheet5"))
         time.sleep(1)
 
+        # wb1.Close(savechanges=0)
+        # wb2.Close(savechanges=1)
+
+        # wb1.Worksheets("Sheet1").Copy(After=wb2.Worksheets("Sheet1"))
+        # wb2.Save()
         wb1.SaveAs(new_file, FileFormat=51)
 
     except Exception as e:
